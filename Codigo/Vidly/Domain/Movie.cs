1aa89aa3ba307fd34905ccb316203ee39cfde96d
Daniel Acevedo <daniel18acevedo@gmail.com>
2021-09-15 10:17:12 -0300

    Adding User class

--- Codigo/Vidly/Domain/Movie.cs
+++ Codigo/Vidly/Domain/Movie.cs
@@ -2,6 +2,8 @@ namespace Domain
 {
     public class Movie
     {
-        
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
     }
 }
